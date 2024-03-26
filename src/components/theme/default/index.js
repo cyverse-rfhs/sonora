3be009f7f78913bdc94857b86b4e057ffd4ee714
mgwall17 <mgwall17@gmail.com>
2024-03-26 11:08:57 -0700

    updated palette and logo for UM RDHS

--- src/components/theme/default/index.js
+++ src/components/theme/default/index.js
@@ -2,7 +2,7 @@ import { getMuiTheme } from "./CyVerseTheme";
 
 const theme = getMuiTheme();
 
-theme.palette.main_sidebar = "#052537";
-theme.palette.main_appbar = "#074060";
+theme.palette.main_sidebar = "#0F3156";
+theme.palette.main_appbar = "#1F64B0";
 
 export default theme;
